d4f3e00c647a1f21d1f9b880aece8dbac12acdfe
daphne <opt004daphnecabrera@gmail.com>
2020-10-06 23:20:41 -0400

    miprimercommit

---
+++ app/src/main/java/com/example/androidcomidarapida/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.androidcomidarapida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView map;
+    private GoogleMap mMap;
+    private Geocoder geocoder;
+    private TextView street;
+    private Button next;
+    private LatLng mainposition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            }
+
+
+
+}
