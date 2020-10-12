c4f060e23d7d1dfe7025ec776ffc36f9ae324311
daphne <opt004daphnecabrera@gmail.com>
2020-10-12 17:44:08 -0400

    mas complementos

--- app/src/main/java/com/example/androidcomidarapida/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/androidcomidarapida/ui/dashboard/DashboardFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,7 +17,10 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.androidcomidarapida.R;
 
-public class DashboardFragment extends Fragment {
+import java.util.ArrayList;
+
+public class DashboardFragment extends Fragment  {
+
 
     private DashboardViewModel dashboardViewModel;
 
@@ -23,13 +29,31 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        final TextView textView = root.findViewById(R.id.text_dashboard); //aqui llamar
+        //root.findViewById(R.id.)
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+
             }
         });
         return root;
+
     }
+
+    public void onStart() {
+        super.onStart();
+        ListView list = this.getActivity().findViewById(R.id.milistamenu);
+        ArrayList<String> datos = new ArrayList<>();
+        for (int i=0; i <= 100 ; i++){
+            datos.add("Item  " + i);
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this.getContext(),android.R.layout.simple_list_item_1, datos);
+        list.setAdapter(adapter);
+    }
+
+
+
+    //aqui camara
 }
